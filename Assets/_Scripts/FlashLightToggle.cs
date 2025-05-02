76639d3b33d015052c1bab25f57a1cebdd831d6b
amtrevisan <alex.morales8@upr.edu>
2025-05-01 21:53:46 -0400

    FlashLight Battery

--- Assets/_Scripts/FlashLightToggle.cs
+++ Assets/_Scripts/FlashLightToggle.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class FlashlightToggle : MonoBehaviour
 {
     private Light flashlight;
+    public float batteryLife = 60f; // Battery duration in seconds
+    private float currentBattery;
 
     void Start()
     {
         flashlight = GetComponent<Light>();
+        currentBattery = batteryLife;
+
         if (flashlight != null)
         {
             flashlight.enabled = false; // Start with flashlight off
@@ -19,11 +23,24 @@ public class FlashlightToggle : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && currentBattery > 0)
         {
             if (flashlight != null)
             {
                 flashlight.enabled = !flashlight.enabled;
+            }
+        }
+
+        // Drain battery if flashlight is on
+        if (flashlight != null && flashlight.enabled)
+        {
+            currentBattery -= Time.deltaTime;
+
+            if (currentBattery <= 0)
+            {
+                currentBattery = 0;
+                flashlight.enabled = false;
+                Debug.Log("Battery dead");
             }
         }
     }
